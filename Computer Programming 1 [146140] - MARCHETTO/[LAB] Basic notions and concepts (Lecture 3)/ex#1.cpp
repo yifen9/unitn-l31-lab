d85ccdd8c8549c955517a33ad4990b8888002b0f
Anto <leeantoyf@gmail.com>
2024-10-28 13:51:33 +0100

    Add files via upload

---
+++ Computer Programming 1 [146140] - MARCHETTO/[LAB] Basic notions and concepts (Lecture 3)/ex#1.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+    float r, pi = 3.14;
+    cout << "Please input radius:" << endl;
+    cin >> r;
+
+    cout << "Volume = " << 4.0/3.0*pi*r*r*r << endl;
+
+    return 0;
+}
