a39534b946668c12e1f2043b5945853459accc18
Clare <leeantoyf@gmail.com>
2024-10-29 23:20:59 +0100

    update

---
+++ Computer_Programming_#1_146140_MARCHETTO/Lecture_#06_LAB_Binary_Representations_and_Operations_with_Exercises/exAtHome.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+    int a = 16;
+    cout << "a: " << a << endl;
+
+    int b = a << 2;
+    cout << "a*4: " << b << endl;
+
+    int c = a >> 1;
+    cout << "a/2: " << c << endl;
+
+    return 0;
+}
