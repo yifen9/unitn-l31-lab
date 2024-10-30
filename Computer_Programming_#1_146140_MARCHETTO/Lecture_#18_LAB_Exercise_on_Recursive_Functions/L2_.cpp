c526f0922eaa7a9b377d408e9f72982760ea7cdb
Anto <leeantoyf@gmail.com>
2024-10-30 15:23:11 +0100

    Add files via upload

---
+++ Computer_Programming_#1_146140_MARCHETTO/Lecture_#18_LAB_Exercise_on_Recursive_Functions/L2_.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+#include <iostream>
+
+int free(int[]);
+
+int add_node(int,int,int,int);
+
+bool is_allocated(int);
+
+int get_node_value(int);
+
+int get_left_child(int);
+
+int get_right_child(int);
+
+bool is_leaf(int);
+
+int main(){
+    int memory[4*1000];
+
+    return 0;
+}
+
+int free(int memory[]){return 0;}
+
+int add_node(int value, int left_child, int right_child, int memory[]){return 0;}
+
+bool is_allocated(int index_of_node){return 0;}
+
+int get_node_value(int index_of_node){return 0;}
+
+int get_left_child(int index_of_node){return 0;}
+
+int get_right_child(int index_of_node){return 0;}
+
+bool is_leaf(int index_of_node){return 0;}
