9184dae4b850a69a8b6ae69fa8501210178cbe0c
Clare <leeantoyf@gmail.com>
2024-11-21 00:13:23 +0100

    Update 2.cpp

--- Computer_Programming_1_146140_MARCHETTO/Lecture_39_LAB_Simulation_of_the_exam/2.cpp
+++ Computer_Programming_1_146140_MARCHETTO/Lecture_39_LAB_Simulation_of_the_exam/2.cpp
@@ -9,33 +9,38 @@ struct node{
 
 int** f1(){
     srand(time(NULL));
-
     const int X = 1+rand()%(0b1111);
 
     int** res = new int*[2];
+
     res[0] = new int;
     res[0][0] = X;
-    res[1] = new int[X];
 
+    res[1] = new int[X];
     for(int i=0; i<X; i++){res[1][i] = rand()%(1+0b11111111);}
 
     return res;
 }
 
 node* f2(const int X, const int* A){
-    node* B = new node();
-    node *p1=B, *p2=p1;
-    for(int i=0; i<X; i++){
-        p1->data = A[i];
-        p2 = new node();
-        p1->next = p2;
-        p2->prev = p1;
-        p1 = p1->next;
-    }
-    p1->next = NULL;
-    delete p2;
+    if(X > 0){
+        node* B = new node();
+        node *p1=B, *p2=p1;
+        for(int i=0; i<X; i++){
+            p1->data = A[i];
+            p2 = new node();
 
-    return B;
+            p1->next = p2;
+            p2->prev = p1;
+            
+            p1 = p1->next;
+        }
+        p1->prev->next = NULL;
+        delete p2;
+
+        return B;
+    }
+    else{return NULL;}
 }
 
 void f3(node* A){
@@ -49,7 +54,43 @@ void f3(node* A){
 }
 
 void f4(node* &A, const int P, const int num){
+    int X;
+    node* t = A;
+    for(X=0; t!=NULL; t=t->next){X++;}
 
+    if(X == 0){
+        A = new node();
+        A->data = num;
+    }
+    else{
+        t = A;
+        if(P == 0){
+            t->prev = new node();
+            t->prev->data = num;
+            t->prev->next = t;
+
+            A = t->prev;
+        }
+        else if(P == X){
+            while(t->next != NULL){t = t->next;}
+
+            t->next = new node();
+            t->next->data = num;
+            t->next->prev = t;
+        }
+        else{
+            for(int i=0; i<P; i++){t = t->next;}
+            node *p1=t->prev, *p2=t;
+
+            t = new node();
+            t->data = num;
+            t->prev = p1;
+            t->next = p2;
+
+            p1->next = t;
+            p2->prev = t;
+        }
+    }
 }
 
 int main(){
@@ -64,6 +105,12 @@ int main(){
     cout << endl;
 
     node* B = f2(X,A);
+    f3(B);
+
+    int P;
+    cout << "P: ";
+    cin >> P;
+    f4(B,P,rand()%(1+0b11111111));
     f3(B);
 
     return 0;
