98fe93a60dce35f95af93eea17bf4ba6afb5bb48
Clare <leeantoyf@gmail.com>
2025-04-15 19:04:56 +0200

    update

--- docs/overrides/extra.js
+++ docs/overrides/extra.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const logo = document.querySelector(".md-header__button.md-logo");
-    if (logo) {
-      logo.setAttribute("onclick", "window.location='https://www.unitn.it';");
-    }
-  });
+  const logo = document.querySelector(".md-header__button.md-logo");
+  if (logo) {
+    logo.onclick = () => {
+      window.location.href = "https://www.unitn.it/";
+    };
+  }
+});
