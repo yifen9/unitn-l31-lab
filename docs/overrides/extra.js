0ed84ca90d28079cea2055886218e243dea7fc5f
Clare <leeantoyf@gmail.com>
2025-04-16 23:26:03 +0200

    update

--- docs/overrides/extra.js
+++ docs/overrides/extra.js
@@ -2,9 +2,23 @@ document$.subscribe(function() {
   var tables = document.querySelectorAll("article table")
   tables.forEach(function(table) {
     new Tablesort(table)
-  })
-})
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logo = document.querySelector(".md-header__button.md-logo");
+  if (logo) {
+    logo.href = "https://webapps.unitn.it/gestionecorsi";
+    logo.setAttribute("target", "_blank");
+    logo.setAttribute("title", "Visit UniTrento homepage");
+  }
+});
 
 document$.subscribe(() => {
-  hljs.highlightAll()
-})
+  document.querySelectorAll('a[href^="http"]').forEach(link => {
+    if (!link.href.includes(location.hostname)) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    }
+  });
+});
