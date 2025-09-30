29f3edf3d51ac360d0cfa31b34ded09f80396bb1
yifen9 <mail@yifen9.li>
2025-09-30 15:09:15 +0200

    update ap l6

---
+++ src/146125_ap/lab/gym/a1/src/q1.rs
@@ -0,0 +1,32 @@
+fn string_reverse_with_rev(s: &str) -> String {
+    let mut out = String::new();
+    for c in s.chars().rev() {
+        out.push(c);
+    }
+    out
+}
+
+fn string_reverse_with_pop_match(s: &str) -> String {
+    let mut out = String::new();
+    let mut s = s.to_string();
+    loop {
+        match s.pop() {
+            None => break,
+            Some(c) => out.push(c),
+        }
+    }
+    out
+}
+
+fn string_reverse_with_pop_while_let(s: &str) -> String {
+    let mut out = String::new();
+    let mut s = s.to_string();
+    while let Some(c) = s.pop() {
+        out.push(c);
+    }
+    out
+}
+
+fn string_reverse_with_collect(s: &str) -> String {
+    s.chars().rev().collect()
+}
---
+++ src/146125_ap/lab/gym/a1/src/q2.rs
@@ -0,0 +1,7 @@
+fn bigger(int1: i32, int2: i32) -> i32 {
+if int1 >= int2 {
+return int1;
+} else {
+return int2;
+}
+}
