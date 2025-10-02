373e451e3e8366746f8b00f8220a9563fbfaa793
yifen9 <mail@yifen9.li>
2025-10-02 17:25:36 +0200

    add ads1 lab1

---
+++ src/146290_ads1/lab/src/l1/e1.c
@@ -0,0 +1,52 @@
+/*
+Player struct
+
+Create a `struct player` that contains
+
+• The name of the player
+• The surname of the player
+• The height of the player
+• The weight of the player
+• The number of wins of the player
+• The number of matches played by the player
+
+Which types should you use for each field?
+
+Write a function `playerScore` that, given a player, returns the score of that player,
+calculated as the percentage of matches that the given player won.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+
+typedef unsigned short int usi;
+
+struct player {
+    char * name;
+    char * surname;
+    usi height;
+    usi weight;
+    usi wins;
+    usi matches;
+};
+
+float usi2f(usi a) {
+    return ((float)a);
+}
+
+float playerScore(struct player P) {
+    return (usi2f(P.wins) / usi2f(P.matches));
+}
+
+int main() {
+    struct player P;
+    P.wins = 2;
+    P.matches = 5;
+    printf("%f\n", playerScore(P));
+
+    return 0;
+}
---
+++ src/146290_ads1/lab/src/l1/e2.c
@@ -0,0 +1,155 @@
+/*
+Arrays, basics
+
+Define a constant `MAX_SIZE = 10`, create a player array of that size, then create
+functions to:
+
+• Add a player in the array
+    • What happens if the array is full?
+• Print the array
+    • What happens if the array is NOT full?
+• Remove a player, given the index inside the array
+    • What happens if the indexed player does not exist?
+• Return the player with the highest score
+    • How do you return it? Just the index, a pointer, a copy, ... you decide.
+
+Briefly discuss the computational complexity of each of the procedures.
+*/
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+
+#define rep(i, n) for(usi i=0; i<n; ++i)
+#define pl printf("\n")
+#define plu(s, u) printf("%s%u\n", s, u)
+#define pls(s1, s2) printf("%s%s\n", s1, s2)
+
+#define MAX_SIZE 10
+
+typedef unsigned short int usi;
+
+struct player {
+    char * name;
+    char * surname;
+    usi height;
+    usi weight;
+    usi wins;
+    usi matches;
+};
+
+struct player * createPlayerArray(usi m) {
+    struct player * PA = NULL;
+    PA = (struct player *) malloc(m * sizeof(struct player));
+    return PA;
+}
+
+void addPlayer(
+    struct player * P,
+    char * name,
+    char * surname,
+    usi height,
+    usi weight,
+    usi wins,
+    usi matches
+) {
+    P->name = name;
+    P->surname = surname;
+    P->height = height;
+    P->weight = weight;
+    P->wins = wins;
+    P->matches = matches;
+}
+
+bool addPlayerInArray(
+    usi m,
+    struct player * PA,
+    char * name,
+    char * surname,
+    usi height,
+    usi weight,
+    usi wins,
+    usi matches
+) {
+    usi i = 0;
+    while((i < m) && PA[i].name) ++i;
+    if(i == m) return false;
+    addPlayer(
+        &PA[i],
+        name,
+        surname,
+        height,
+        weight,
+        wins,
+        matches
+    );
+    return true;
+}
+
+void printPlayer(struct player P) {
+    pls("name:    ", P.name);
+    pls("surname: ", P.surname);
+    plu("height:  ", P.height);
+    plu("weight:  ", P.weight);
+    plu("wins:    ", P.wins);
+    plu("matches: ", P.matches);
+}
+
+void printPlayerArray(usi m, struct player * PA) {
+    usi i = 0;
+    while((i < m) && PA[i].name) {
+        plu("INDEX: ", i);
+        printPlayer(PA[i]);
+        ++i;
+    }
+}
+
+bool removePlayer(usi m, struct player * PA, usi i) {
+    if(i >= m) return false;
+    free(PA[i]);
+    PA[i] = (struct player *) malloc(sizeof(struct player));
+    return true;
+}
+
+int main() {
+    struct player * PA = createPlayerArray(MAX_SIZE);
+
+    addPlayerInArray(
+        MAX_SIZE,
+        PA,
+        "Yifeng",
+        "Li",
+        175,
+        50,
+        4,
+        5
+    );
+    addPlayerInArray(
+        MAX_SIZE,
+        PA,
+        "Yifeng",
+        "Li",
+        175,
+        50,
+        4,
+        5
+    );
+    addPlayerInArray(
+        MAX_SIZE,
+        PA,
+        "Yifeng",
+        "Li",
+        175,
+        50,
+        4,
+        5
+    );
+
+    printPlayerArray(MAX_SIZE, PA);
+
+    return 0;
+}
