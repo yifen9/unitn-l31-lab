23c4c061749e7785ad28963c60199e69994e4ce2
yifen9 <mail@yifen9.li>
2025-10-13 02:25:43 +0200

    up to ap book c4-2

---
+++ src/146125_ap/lab/book/c4-2c/src/main.rs
@@ -0,0 +1,26 @@
+/*
+Dangling References
+*/
+
+#![allow(unused)]
+fn main() {
+    // let reference_to_nothing = dangle();
+
+    let reference_to_nothing = no_dangle();
+}
+
+/*
+fn dangle() -> &String { // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope and is dropped, so its memory goes away.
+  // Danger!
+*/
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}
